dd9f2f48aaac00d9ded6cf4d9f77d527101b70f4
karkouri-zakaria <92269313+karkouri-zakaria@users.noreply.github.com>
2022-07-03 19:47:28 +0100

    Update views.py

--- Home/views.py
+++ Home/views.py
@@ -1,4 +1,3 @@
-from email import message
 from django.template import loader
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
@@ -9,27 +8,21 @@ from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, authenticate, login, logout
 from .models import Movies_List
 
-
-
-def index(request):
-    template = loader.get_template('Main.html')
-    return HttpResponse(template.render({}, request))
-
 page=1
 
-def Popular(request):
-    global page
-    data = request.POST
-    action = data.get("Page_num")
-    if data.get("number") != None:
-        page = data.get("number")
-    elif action == None:
-        page=1
+def index(request):
+    return HttpResponse(loader.get_template('Main.html').render({}, request))
+
+def page_management(request):
+    global page; page=int(request.POST.get('number', page)); action = request.POST.get("Page_num")
+    if action =="Next" :
+        page+=1
     elif action == "Previous" and page>1:
         page-=1
-    elif action =="Next" :
-        page+=1
-    
+    return(page)
+
+def Popular(request):
+    page=page_management(request)
     param={'page':str(page)}
     Movies = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=6fe2c9251ad61629064389bb48013886",params=param).json()['results']
     template = loader.get_template('Grid.html')
@@ -38,29 +31,15 @@ def Popular(request):
         'page' : page,
         'active1':'active',
     }
-
     return HttpResponse(template.render(context, request))
 
 
 def searching(request):
-    data = request.GET
-    action = data.get("search")
-    if action != None and action != '': 
-        global page
-        data = request.POST
-        action2 = data.get("Page_num")
-        if data.get("number") != None:
-            page = data.get("number")
-        elif action2 == None:
-            page=1
-        elif action2 == "Previous" and page>1:
-            page-=1
-        elif action2 =="Next" :
-            page+=1
-
+    data = request.GET; action = data.get("search")
+    if action != '': 
+        page=page_management(request)
         param={'page':str(page),'query':action}
         Movies = requests.get("https://api.themoviedb.org/3/search/multi?api_key=6fe2c9251ad61629064389bb48013886",params=param).json()['results']
-
         template = loader.get_template('Grid.html')
         context = { 
             'Movies': Movies,
@@ -68,22 +47,10 @@ def searching(request):
         }
         return HttpResponse(template.render(context, request))
     else :
-        return HttpResponseRedirect(reverse('index'))
+        return HttpResponseRedirect(reverse('s_p'))
 
 def Top_rated(request):
-    global page
-    data = request.POST
-    action = data.get("Page_num")
-    if data.get("number") != None:
-        page = data.get("number")
-    elif action == None:
-        page=1
-    elif action == "Previous" and page>1:
-        page-=1
-    elif action =="Next" :
-        page+=1
-
-    
+    page=page_management(request)
     param={'page':str(page)}
     Movies = requests.get("https://api.themoviedb.org/3/movie/top_rated?api_key=6fe2c9251ad61629064389bb48013886",params=param).json()['results']
     template = loader.get_template('Grid.html')
@@ -92,46 +59,31 @@ def Top_rated(request):
         'page' : page,
         'active2': 'active',
     }
-
     return HttpResponse(template.render(context, request))
 
 
 def Contact(request):
-    template = loader.get_template('contact.html')
-    return HttpResponse(template.render({'active5':'active',}, request))
+    return HttpResponse(loader.get_template('contact.html').render({'active5':'active',}, request))
 
 def Search_page(request):
-    template = loader.get_template('Search_page.html')
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(loader.get_template('Search_page.html').render({}, request))
 
 def Movie(request):
+    from random import shuffle
     data = request.GET
     List=Movies_List.objects.all().values()
     if bool(request.POST.get('Fav_button', False)):
-        if request.user.is_authenticated and not any([True for dic in List  if dic['movie']==int(data['id']) and dic['username_id'] == request.user.id]):
+        if not any([True for dic in List  if dic['movie']==int(data['id']) and dic['username_id'] == request.user.id]):
             Movies_List(movie=data['id'],username_id=request.user.id).save()
         else:
             Movies_List.objects.get(movie=int(data['id']),username_id=request.user.id).delete()
 
-    from random import shuffle
-    global page
-
     id=data.get("id")
-    data = request.POST
-    action = data.get("Page_num")
-    if data.get("number") != None:
-        page = data.get("number")
-    elif action == None:
-        page=1
-    elif action == "Previous" and page>1:
-        page-=1
-    elif action =="Next" :
-        page+=1
-
+    page=page_management(request)
     param={'page':str(page)}
     template = loader.get_template('Movie.html')
 
-    Fav ="favorited" if id!=None and any([True for dic in List  if dic['movie']==int(id) and dic['username_id'] == request.user.id]) else ""
+    Fav ="favorited" if id is not None and any([True for dic in List  if dic['movie']==int(id) and dic['username_id'] == request.user.id]) else ""
 
     Main_Movie = requests.get("https://api.themoviedb.org/3/movie/"+str(id)+"?api_key=6fe2c9251ad61629064389bb48013886").json()
     Trailers = requests.get("https://api.themoviedb.org/3/movie/"+str(id)+"/videos?api_key=6fe2c9251ad61629064389bb48013886").json()['results']
@@ -161,8 +113,7 @@ def Connect(request):
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
+            username = form.cleaned_data.get('username'); password = form.cleaned_data.get('password')
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
@@ -198,33 +149,18 @@ def addUser(request):
 
 
 def Reset(request):
-    template = loader.get_template('Lost_pass.html')
-    return HttpResponse(template.render({}, request))
+    return HttpResponse(loader.get_template('Lost_pass.html').render({}, request))
 
 def Return(request):
     from pathlib import Path
-    BASE_DIR = Path(__file__).resolve().parent.parent
-    file=open(str(BASE_DIR)+'\\static\\txt\\password_reset_email.txt',"a")
-    file.write(request.POST.get('email'))
-    file.write("\n")
+    file=open(str(Path(__file__).resolve().parent.parent)+'\\static\\txt\\password_reset_email.txt',"a")
+    file.write(request.POST.get('email')); file.write("\n")
     file.close()
     return HttpResponseRedirect(reverse('index'))
 
-def MyList(request):
-    global page
-    data = request.POST
-    action = data.get("Page_num")
-    page=int(page)
-    if data.get("number") != None:
-        page = int(data.get("number"))
-    elif action == None:
-        page=1
-    elif action == "Previous" and page>1:
-        page-=1
-    elif action =="Next" :
-        page+=1
+def MyList(request,Movies=[]):
+    page=page_management(request)
     List=Movies_List.objects.all().values()
-    Movies=[]
     for dic in List[10*(page-1):10*page]:
         if dic["username_id"]==request.user.id:
             Movies.append(requests.get("https://api.themoviedb.org/3/movie/"+str(dic['movie'])+"?api_key=6fe2c9251ad61629064389bb48013886").json())
@@ -236,15 +172,14 @@ def MyList(request):
     }
     return HttpResponse(template.render(context, request))
 
-def Suggestions(request):
+def Suggestions(request,Movies=[]):
     from random import shuffle,randint
     List=Movies_List.objects.all().values()
-    param={'page':randint(1,20)}
-    Movies=[]
+    param={'page':randint(1,30)}
     for dic in List:
         if dic["username_id"]==request.user.id:
-            SL=requests.get("https://api.themoviedb.org/3/movie/"+str(dic['movie'])+"/similar?api_key=6fe2c9251ad61629064389bb48013886&sort_by=vote_average.desc",params=param).json()['results']
-            for movie in SL:
+            Similars_List=requests.get("https://api.themoviedb.org/3/movie/"+str(dic['movie'])+"/similar?api_key=6fe2c9251ad61629064389bb48013886&sort_by=vote_average.desc",params=param).json()['results']
+            for movie in Similars_List:
                 if float(movie["vote_average"])>=7:
                     Movies.append(movie)
     shuffle(Movies)
@@ -258,12 +193,10 @@ def Suggestions(request):
 
 def Contacting(request):
     from pathlib import Path
-    BASE_DIR = Path(__file__).resolve().parent.parent
-    file=open(str(BASE_DIR)+'\\static\\txt\\Contact_msgs.txt',"a")
+    file=open(str(Path(__file__).resolve().parent.parent)+'\\static\\txt\\Contact_msgs.txt',"a")
     for k,v in request.POST.items():
         file.write(f"{k} : {v} \n")
     file.write("---------------------------------------------------\n")
     file.close()
     messages.info(request,f"{request.POST['name']} we have received your message successfully, Thank you for contacting us. With love SMM's team")
     return HttpResponseRedirect(reverse('contact'))
-
